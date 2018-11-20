eb86d1be6903f4f834129ff62d3313a466a5a93d
Andrey <Andrey.Sakhno@gmail.com>
2018-11-20 08:34:54 +0200

    Add flask-admin

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
 
+# config for app
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+# config for flask-admin
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
