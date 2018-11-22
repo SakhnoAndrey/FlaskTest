c64b178ef84bb9ba1039abae84a7b87397f6f4c7
Andrey <Andrey.Sakhno@gmail.com>
2018-11-22 16:23:54 +0200

    Error of db

--- admin/views.py
+++ admin/views.py
@@ -3,6 +3,5 @@ from app import app, db
 from app.models import User, Post
 from flask_admin.contrib.sqla import ModelView
 
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Post, db.session))
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from app.models import User, Post
 
 # config for app
 app = Flask(__name__)
@@ -15,3 +17,6 @@ from app import routes, models
 # config for flask-admin
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Post, db.session))
