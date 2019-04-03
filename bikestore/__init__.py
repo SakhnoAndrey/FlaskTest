2fbc77f9f9849d623df3e00ca60981784dc59edc
Andrey <Andrey.Sakhno@gmail.com>
2019-04-03 16:37:35 +0300

    Error handling without log to email

--- bikestore/__init__.py
+++ bikestore/__init__.py
@@ -2,4 +2,36 @@ from .application import flask_app, db
 from .admin import admin
 from .routes import index, login
 from . import errors
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
+if not flask_app.debug:
+    if flask_app.config['MAIL_SERVER']:
+        auth = None
+        if flask_app.config['MAIL_USERNAME'] or flask_app.config['MAIL_PASSWORD']:
+            auth = (flask_app.config['MAIL_USERNAME'], flask_app.config['MAIL_PASSWORD'])
+        secure = None
+        if flask_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(flask_app.config['MAIL_SERVER'], flask_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + flask_app.config('MAIL_SERVER'),
+            toaddrs=flask_app.config['ADMINS'],
+            subject='BikeStore failure',
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        flask_app.logger.addHandler(mail_handler)
+
+if not flask_app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/bikestore.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    flask_app.logger.addHandler(file_handler)
+    flask_app.logger.setLevel(logging.INFO)
+    flask_app.logger.info('BikeStore startup')
--- config.py
+++ config.py
@@ -10,3 +10,10 @@ class Config(object):
     #SQLALCHEMY_DATABASE_URI = 'postgresql://lw:flask@localhost/flask_test'
     SQLALCHEMY_DATABASE_URI = 'postgres://yukfxrte:PkM5ABnQl--7xGWsjH8_yPcwAuxg6tVz@balarama.db.elephantsql.com:5432/yukfxrte'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['andrey.sakhno@gmail.com']
