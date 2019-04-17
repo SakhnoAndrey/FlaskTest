533658dea6996bc4001babd3e2508cb44997ddc4
Andrey <Andrey.Sakhno@gmail.com>
2019-04-17 15:57:46 +0300

    Pagination is over

--- bikestore/routes.py
+++ bikestore/routes.py
@@ -7,8 +7,8 @@ from .forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from .models import User, Post
 
 
-@flask_app.route('/')
-@flask_app.route('/index')
+@flask_app.route('/', methods=['GET', 'POST'])
+@flask_app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
@@ -19,9 +19,17 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, flask_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
     # Непонятно откуда взять posts принадлежащее конкретному пользователю
-    return render_template('index.html', title='Home Page', posts=posts)
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @flask_app.route('/login', methods=['GET', 'POST'])
@@ -41,10 +49,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @flask_app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @flask_app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -60,21 +70,29 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @flask_app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, flask_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
 
 @flask_app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @flask_app.route('/edit_profile', methods=['GET', "POST"])
 @login_required
@@ -91,6 +109,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
+
 @flask_app.route('/follow/<username>')
 @login_required
 def follow(username):
@@ -105,6 +124,7 @@ def follow(username):
     db.session.commit()
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
+
 
 @flask_app.route('/unfollow/<username>')
 @login_required
@@ -121,8 +141,17 @@ def unfollow(username):
     flash('You are not following {}!'.format(username))
     return redirect(url_for('user', username=username))
 
+
 @flask_app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, flask_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
