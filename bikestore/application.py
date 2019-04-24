24b0afaa4d4a5d70833eba76d186b97d178c7643
Andrey <Andrey.Sakhno@gmail.com>
2019-04-24 14:32:03 +0300

    Added flask-bootstrap

--- bikestore/application.py
+++ bikestore/application.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 # config for app
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
 mail = Mail(flask_app)
+bootstrap = Bootstrap(flask_app)
 
 db = SQLAlchemy(flask_app)
 migrate = Migrate(flask_app, db)
