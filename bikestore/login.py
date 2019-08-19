d646ef86bb4b9beefe5f5997619066eb4b3beff8
Andrey <Andrey.Sakhno@gmail.com>
2019-08-19 14:22:19 +0300

    Changed login.py

--- bikestore/login.py
+++ bikestore/login.py
@@ -1,5 +1,7 @@
 from flask_login import LoginManager
+from flask_babel import lazy_gettext as _l
 from .application import flask_app
 
 login = LoginManager(flask_app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
